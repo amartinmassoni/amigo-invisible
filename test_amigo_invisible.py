8fd7c2df85fa952d14d15c546af228100bd18664
Aurelio Martin <amartinmassoni@gmail.com>
2021-12-14 08:02:47 +0100

    Mas tests

--- test_amigo_invisible.py
+++ test_amigo_invisible.py
@@ -10,10 +10,26 @@ def test_tipo_lista():
 
 def test_longitud_lista():
     lista = leer_lista( FICHERO_PRUEBAS )
-    assert len( lista ) == 3
+    assert len( lista ) == 5
+
+def test_items_lista():
+    lista = leer_lista( FICHERO_PRUEBAS )
+    for item in lista:
+        assert type( item ) == list
+        assert len( item ) == 2
 
 def test_emparejar1():
     lista = leer_lista( FICHERO_PRUEBAS )
     pares = emparejar_lista( lista )
-    assert len( pares ) == 3
+    assert len( pares ) == len( lista )
+
+def test_emparejar2():
+    lista = leer_lista( FICHERO_PRUEBAS )
+    pares1 = emparejar_lista( lista )
+    pares2 = emparejar_lista( lista )
+    pares1.sort()
+    pares2.sort()
+    texto1 = "\n".join( [ str( p ) for p in pares1 ] )
+    texto2 = "\n".join( [ str( p ) for p in pares2 ] )
+    assert texto1 != texto2
 
