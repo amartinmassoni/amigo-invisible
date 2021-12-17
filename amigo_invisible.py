b01a53822cb28fd65edd601f67f02819b8a25eb7
Aurelio Martin <amartinmassoni@gmail.com>
2021-12-17 18:24:20 +0100

    Cambios sugeridos por flake8

--- amigo_invisible.py
+++ amigo_invisible.py
@@ -2,50 +2,50 @@
 
 import random
 import argparse
-from enviar_email import enviar_email
 
-def emparejar_lista( lista ):
+
+def emparejar_lista(lista):
     compran = lista[:]
     reciben = lista[:]
-    random.shuffle( compran )
-    for i in range( 10 ):
-        random.shuffle( reciben )
-        resultado = list( zip( compran, reciben ) )
-        if not [ ( r1, r2 ) for ( r1, r2 ) in resultado if r1 == r2 ]:
+    random.shuffle(compran)
+    for i in range(10):
+        random.shuffle(reciben)
+        resultado = list(zip(compran, reciben))
+        if not [(r1, r2) for (r1, r2) in resultado if r1 == r2]:
             resultado.sort()
             return resultado
     raise ValueError
 
-def leer_lista( nombrefichero ):
-    return [ [ item for item in linea.strip().split( '\t' ) if item ]
-             for linea in open( nombrefichero, 'r' ) ]
 
-def leer_conf_email( nombrefichero ):
-    return dict( [ [ item.strip() for item in linea.split( '=', 1 ) ] 
-             for linea in open( nombrefichero, 'r' ) ] )
+def leer_lista(nombrefichero):
+    return [[item for item in linea.strip().split('\t') if item]
+            for linea in open(nombrefichero, 'r')]
+
+
+def leer_conf_email(nombrefichero):
+    return dict([[item.strip() for item in linea.split('=', 1)]
+                for linea in open(nombrefichero, 'r')])
+
 
 if __name__ == '__main__':
-    import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument( 'lista',
-        help = 'Fichero con la lista de participantes (ejemplo lista_test.txt)' )
-    parser.add_argument( '--envio',
-        help = 'Fichero con la configuracion de email (ejemplo email_test.txt)' )
+    parser.add_argument('lista',
+                        help='Lista de participantes (lista_test.txt)')
+    parser.add_argument('--envio',
+                        help='Configuracion de email (email_test.txt)')
     args = parser.parse_args()
 
     if args.envio:
-        conf_email = leer_conf_email( args.envio )
-    personas = leer_lista( args.lista )
-    resultado = emparejar_lista( personas )
-    for ( regalan, reciben ) in resultado:
-        print( regalan[ 0 ], "regalan a", reciben[ 0 ] )
+        conf_email = leer_conf_email(args.envio)
+    personas = leer_lista(args.lista)
+    resultado = emparejar_lista(personas)
+    for (regalan, reciben) in resultado:
+        print(regalan[0], "regalan a", reciben[0])
         if conf_email:
-            print( "======= email para", regalan[ 1 ], "=========" )
-            print( "From", conf_email['de'] )
-            print( "To", regalan[1] )
-            print( "Subject", conf_email['asunto'] )
-            print( eval( conf_email['cuerpo'] ) )
-            print( "=============================================" )
-
-
+            print("======= email para", regalan[1], "=========")
+            print("From", conf_email['de'])
+            print("To", regalan[1])
+            print("Subject", conf_email['asunto'])
+            print(eval(conf_email['cuerpo']))
+            print("=============================================")
--- test_amigo_invisible.py
+++ test_amigo_invisible.py
@@ -5,38 +5,44 @@ import amigo_invisible
 FICHERO_PRUEBAS = 'lista_test.txt'
 FICHERO_CONF_EMAIL = 'email_test.txt'
 
+
 def test_tipo_lista():
-    lista = amigo_invisible.leer_lista( FICHERO_PRUEBAS )
-    assert type( lista ) == list
+    lista = amigo_invisible.leer_lista(FICHERO_PRUEBAS)
+    assert type(lista) == list
+
 
 def test_longitud_lista():
-    lista = amigo_invisible.leer_lista( FICHERO_PRUEBAS )
-    assert len( lista ) == 5
+    lista = amigo_invisible.leer_lista(FICHERO_PRUEBAS)
+    assert len(lista) == 5
+
 
 def test_items_lista():
-    lista = amigo_invisible.leer_lista( FICHERO_PRUEBAS )
+    lista = amigo_invisible.leer_lista(FICHERO_PRUEBAS)
     for item in lista:
-        assert type( item ) == list
-        assert len( item ) == 2
+        assert type(item) == list
+        assert len(item) == 2
+
 
 def test_emparejar1():
-    lista = amigo_invisible.leer_lista( FICHERO_PRUEBAS )
-    pares = amigo_invisible.emparejar_lista( lista )
-    assert len( pares ) == len( lista )
+    lista = amigo_invisible.leer_lista(FICHERO_PRUEBAS)
+    pares = amigo_invisible.emparejar_lista(lista)
+    assert len(pares) == len(lista)
+
 
 def test_emparejar2():
-    lista = amigo_invisible.leer_lista( FICHERO_PRUEBAS )
-    pares1 = amigo_invisible.emparejar_lista( lista )
-    pares2 = amigo_invisible.emparejar_lista( lista )
+    lista = amigo_invisible.leer_lista(FICHERO_PRUEBAS)
+    pares1 = amigo_invisible.emparejar_lista(lista)
+    pares2 = amigo_invisible.emparejar_lista(lista)
     pares1.sort()
     pares2.sort()
-    texto1 = "\n".join( [ str( p ) for p in pares1 ] )
-    texto2 = "\n".join( [ str( p ) for p in pares2 ] )
+    texto1 = "\n".join([str(p) for p in pares1])
+    texto2 = "\n".join([str(p) for p in pares2])
     assert texto1 != texto2
 
+
 def test_conf_email1():
-    conf_email = amigo_invisible.leer_conf_email( FICHERO_CONF_EMAIL )
-    assert type( conf_email ) == dict
+    conf_email = amigo_invisible.leer_conf_email(FICHERO_CONF_EMAIL)
+    assert type(conf_email) == dict
     assert "de" in conf_email
     assert "cc" in conf_email
     assert "cco" in conf_email
@@ -45,4 +51,3 @@ def test_conf_email1():
     assert "servidor" in conf_email
     assert "usuario" in conf_email
     assert "password" in conf_email
-
